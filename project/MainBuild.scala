d546c45e990f613d9c21be93ee1de367016ca885
Agny <zyphon6@gmail.com>
2017-07-16 19:14:52 +0300

    add akka-http

--- project/MainBuild.scala
+++ project/MainBuild.scala
@@ -10,7 +10,9 @@ object MainBuild extends Build {
   )
   lazy val json4sJackson = "org.json4s" %% "json4s-jackson" % "3.5.2"
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.1" % "test" exclude("org.scala-lang", "scala-reflect")
-  lazy val deps = Seq(json4sJackson, scalatest)
+  lazy val akkaHttp = "com.typesafe.akka" %% "akka-http" % "10.0.9"
+
+  lazy val deps = Seq(json4sJackson, scalatest, akkaHttp)
 
   lazy val root = project.in(file(".")).settings(commonSettings: _*).settings(libraryDependencies ++= deps)
 }
