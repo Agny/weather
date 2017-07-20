02b540ebd7c9e2cc15450a93886e77794ce0779c
Agny <zyphon6@gmail.com>
2017-07-20 14:38:38 +0300

    DateFormatter is responsible for formatter creation

--- project/MainBuild.scala
+++ project/MainBuild.scala
@@ -10,9 +10,10 @@ object MainBuild extends Build {
   )
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.1" % "test" exclude("org.scala-lang", "scala-reflect") exclude("org.scala-lang.modules", "scala-xml_2.12")
   lazy val akkaHttp = "com.typesafe.akka" %% "akka-http" % "10.0.9"
+  lazy val akkaCors = "ch.megard" %% "akka-http-cors" % "0.2.1"
   lazy val akkaSpray = "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.9"
 
-  lazy val deps = Seq(scalatest, akkaHttp, akkaSpray)
+  lazy val deps = Seq(scalatest, akkaHttp, akkaCors, akkaSpray)
 
   lazy val root = project.in(file(".")).settings(commonSettings: _*).settings(libraryDependencies ++= deps)
 }
--- src/main/scala/ru/agny/weather/utils/DateFormatter.scala
+++ src/main/scala/ru/agny/weather/utils/DateFormatter.scala
@@ -1,15 +1,23 @@
 package ru.agny.weather.utils
 
-import java.text.DateFormat
+import java.text.{SimpleDateFormat, DateFormat}
+import java.util.TimeZone
 
 import ru.agny.weather.utils.UserType.{DateStamp, DateString}
 
 object DateFormatter {
   private val millisInDay = 24 * 60 * 60 * 1000
+  private val dateFormat = "yyyy-MM-dd"
+
+  def createFormatter: DateFormat = {
+    val format = new SimpleDateFormat(dateFormat)
+    format.setTimeZone(TimeZone.getTimeZone("Utc"))
+    format
+  }
 
   implicit def toStamp(date: DateString)(implicit format: DateFormat): DateStamp = format.parse(date).getTime
 
-  def stepByDay(from: DateString, to: DateString)(implicit format: DateFormat): Vector[DateStamp] = {
-    toStamp(from) to toStamp(to) by millisInDay toVector
+  def stepByDay(from: DateStamp, to: DateStamp)(implicit format: DateFormat): Vector[DateStamp] = {
+    from to to by millisInDay toVector
   }
 }
