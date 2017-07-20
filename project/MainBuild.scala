a75f76b76cc4e0bcd81e167d8ddb271b3a5e9c1e
Agny <zyphon6@gmail.com>
2017-07-21 02:15:04 +0300

    add assembly plugin

--- project/MainBuild.scala
+++ project/MainBuild.scala
@@ -1,19 +1,26 @@
 import sbt._
 import Keys._
+import sbtassembly.{AssemblyKeys, AssemblyPlugin}
 
 object MainBuild extends Build {
 
+  import AssemblyKeys._
+
   lazy val commonSettings = Seq(
     organization := "ru.agny",
-    version := "0.1.0",
+    name := "weather-utils",
+    version := "0.1.1",
     scalaVersion := "2.12.2"
   )
-  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.1" % "test" exclude("org.scala-lang", "scala-reflect") exclude("org.scala-lang.modules", "scala-xml_2.12")
   lazy val akkaHttp = "com.typesafe.akka" %% "akka-http" % "10.0.9"
   lazy val akkaCors = "ch.megard" %% "akka-http-cors" % "0.2.1"
   lazy val akkaSpray = "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.9"
 
-  lazy val deps = Seq(scalatest, akkaHttp, akkaCors, akkaSpray)
+  lazy val deps = Seq(akkaHttp, akkaCors, akkaSpray)
 
-  lazy val root = project.in(file(".")).settings(commonSettings: _*).settings(libraryDependencies ++= deps)
+  lazy val root = project.in(file("."))
+    .settings(commonSettings: _*).settings(libraryDependencies ++= deps)
+    .enablePlugins(AssemblyPlugin).settings(
+    mainClass in assembly := Some("ru.agny.weather.WebServer")
+  )
 }
