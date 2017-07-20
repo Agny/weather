5939eb6fb787489156fd908ebd93890161cf7382
Agny <zyphon6@gmail.com>
2017-07-20 14:37:33 +0300

    change winddirdegree to direction

--- src/main/scala/ru/agny/weather/dto/WWOHour.scala
+++ src/main/scala/ru/agny/weather/dto/WWOHour.scala
@@ -4,6 +4,6 @@ import ru.agny.weather.utils.UserType
 import ru.agny.weather.{Wind, HourlyUnit}
 import UserType.HourString
 
-case class WWOHour(time: HourString, tempC: String, windspeedKmph: String, winddirDegree: String, humidity: String) {
-  def toHourlyUnit: HourlyUnit = HourlyUnit(time, tempC.toInt, humidity.toInt, Wind(windspeedKmph.toInt, winddirDegree.toInt))
+case class WWOHour(time: HourString, tempC: String, windspeedKmph: String, winddir16Point: String, humidity: String) {
+  def toHourlyUnit: HourlyUnit = HourlyUnit(time, tempC.toInt, humidity.toInt, Wind(windspeedKmph.toInt, winddir16Point))
 }
